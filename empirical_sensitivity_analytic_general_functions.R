e501a767f7ec95acdc28f0964ce384693a2c9129
yibaizhao <yibaizhao@umass.edu>
2021-12-08 14:56:44 -0800

    Fix some bugs and add functions for mixture model for indolent cases

--- empirical_sensitivity_analytic_general_functions.R
+++ empirical_sensitivity_analytic_general_functions.R
@@ -1,5 +1,5 @@
 # integral
-integrand.screen.detect <- function(u, screen.times, k, lambda, st_params, funs){
+integrand.screen.detect <- function(u, screen.times, k, lambda, st_params, pi, funs){
   ########################################################################################
   #Author: Yibai Zhao
   #This function calculate the probability of screen detection at time u
@@ -9,21 +9,22 @@ integrand.screen.detect <- function(u, screen.times, k, lambda, st_params, funs)
   #        k: k^{th} screen time, t_k
   #        lambda: mean pre-clinical onset time
   #        st_params: parameters for sojourn time function
+  #        pi: probability of being indolent, such that F*=pi+(1-pi)*F --> S*=(1-pi)S
   #        funs: function of sojourn time
   #OUTPUTS: numeric value between 0 and 1
   ##########################################################################################
   
   if(funs == 'exp'){
-    return(dexp(u, rate=lambda)*pexp(screen.times[k]-u, rate=st_params,lower.tail=F))
+    return(dexp(u, rate=lambda)*(1-pi)*pexp(screen.times[k]-u, rate=st_params,lower.tail=F))
   }
   if(funs == 'weibull'){
-    return(dexp(u, rate=lambda)*pweibull(screen.times[k]-u, shape=st_params[1], scale=st_params[2],lower.tail=F))
+    return(dexp(u, rate=lambda)*(1-pi)*pweibull(screen.times[k]-u, shape=st_params[1], scale=st_params[2],lower.tail=F))
   }
   
 }
 
 
-integrand.interval.cancer.false.negative <- function(u, screen.times, k, post.screen.lookout, lambda, st_params, funs){
+integrand.interval.cancer.false.negative <- function(u, screen.times, k, post.screen.lookout, lambda, st_params, pi, funs){
   ########################################################################################
   #Author: Yibai Zhao
   #This function calculate the probability of interval cancer when onset at time u and k^{th} test is false negative
@@ -34,24 +35,25 @@ integrand.interval.cancer.false.negative <- function(u, screen.times, k, post.sc
   #        post.screen.lookout: post screen lookout time
   #        lambda: mean pre-clinical onset time from exp(lambda)
   #        st_params: parameters for sojourn time function
+  #        pi: probability of being indolent, such that F*=pi+(1-pi)*F --> S*=(1-pi)S
   #        funs: function of sojourn time
   #OUTPUTS: numeric value between 0 and 1
   ##########################################################################################
   
   screen.times_k_1 <- screen.times[k] + post.screen.lookout
   if(funs == 'exp'){
-    return(dexp(u, rate=lambda)*( pexp(screen.times_k_1-u, rate=st_params) - pexp(screen.times[k]-u, rate=st_params) ))
+    return(dexp(u, rate=lambda)*( (1-pi)*pexp(screen.times_k_1-u, rate=st_params) - (1-pi)*pexp(screen.times[k]-u, rate=st_params) ))
   }
   if(funs == 'weibull'){
     return(dexp(u, rate=lambda)*( 
-      pweibull(screen.times_k_1-u, shape=st_params[1], scale=st_params[2]) - 
-        pweibull(screen.times[k]-u, shape=st_params[1], scale=st_params[2]) 
+      (1-pi)*pweibull(screen.times_k_1-u, shape=st_params[1], scale=st_params[2]) - 
+        (1-pi)*pweibull(screen.times[k]-u, shape=st_params[1], scale=st_params[2]) 
       ))
   }
   
 }
 
-integrand.interval.cancer.true<-function(u, screen.times, k, post.screen.lookout, lambda, st_params, funs){
+integrand.interval.cancer.true<-function(u, screen.times, k, post.screen.lookout, lambda, st_params, pi, funs){
   ########################################################################################
   #Author: Yibai Zhao
   #This function calculate the probability of interval cancer when onset at time u and k^{th} screen test is true negative
@@ -62,21 +64,22 @@ integrand.interval.cancer.true<-function(u, screen.times, k, post.screen.lookout
   #        post.screen.lookout: post screen lookout time
   #        lambda: mean pre-clinical onset time from exp(lambda)
   #        st_params: parameters for sojourn time function
+  #        pi: probability of being indolent, such that F*=pi+(1-pi)*F --> S*=(1-pi)S
   #        funs: function of sojourn time
   #OUTPUTS: numeric value between 0 and 1
   ##########################################################################################
   
   screen.times_k_1 <- screen.times[k] + post.screen.lookout
   if(funs == 'exp'){
-    return(dexp(u, rate=lambda)*pexp(screen.times_k_1-u, rate=st_params, lower.tail=T))
+    return(dexp(u, rate=lambda)*(pi + (1-pi)*pexp(screen.times_k_1-u, rate=st_params, lower.tail=T)))
   }
   if(funs == 'weibull'){
-    return(dexp(u, rate=lambda)*pweibull(screen.times_k_1-u, shape=st_params[1], scale=st_params[2], lower.tail=T))
+    return(dexp(u, rate=lambda)*(pi + (1-pi)*pweibull(screen.times_k_1-u, shape=st_params[1], scale=st_params[2], lower.tail=T)))
   }
   
 }
 
-screen.detect.time.k <- function(screen.times, k, lambda, st_params, funs, 
+screen.detect.time.k <- function(screen.times, k, lambda, st_params, pi, funs, 
                                  sensitivity){
   ########################################################################################
   #Author: Yibai Zhao
@@ -87,6 +90,7 @@ screen.detect.time.k <- function(screen.times, k, lambda, st_params, funs,
   #        lambda: mean pre-clinical onset time from exp(lambda)
   #        st_params: parameters for sojourn time function
   #        funs: function of sojourn time
+  #        pi: probability of being indolent, such that F*=pi+(1-pi)*F --> S*=(1-pi)S
   #        sensitivity: true sensitivity
   #OUTPUTS: numeric value between 0 and 1
   ##########################################################################################
@@ -96,7 +100,7 @@ screen.detect.time.k <- function(screen.times, k, lambda, st_params, funs,
   if(k == 1){
     screen.detect <- screen.detect + integrate(integrand.screen.detect,
                                                lower=0, upper=screen.times[k], 
-                                               screen.times, k, lambda, st_params, funs)$value*sensitivity
+                                               screen.times, k, lambda, st_params, pi, funs)$value*sensitivity
     return(screen.detect)
   }
   # k>0 ####
@@ -105,17 +109,17 @@ screen.detect.time.k <- function(screen.times, k, lambda, st_params, funs,
     screen.times_i_1 <- ifelse(i == 1, 0, screen.times[i-1])
     screen.detect <- screen.detect + integrate(integrand.screen.detect,
                                                lower=screen.times_i_1, upper=screen.times[i], 
-                                               screen.times, k, lambda, st_params, funs)$value*(1-sensitivity)^(k-i)*sensitivity
+                                               screen.times, k, lambda, st_params, pi, funs)$value*(1-sensitivity)^(k-i)*sensitivity
   }
   # case 2: true negative before k
   screen.detect <- screen.detect + integrate(integrand.screen.detect,
                                              lower=screen.times[k-1], upper=screen.times[k], 
-                                             screen.times, k, lambda, st_params, funs)$value*sensitivity
+                                             screen.times, k, lambda, st_params, pi, funs)$value*sensitivity
   return(screen.detect)
 }
 
 interval.cancer.time.k <- function(screen.times, k, 
-                                   lambda, st_params, funs,
+                                   lambda, st_params, pi, funs,
                                    sensitivity,
                                    post.screen.lookout){
   ########################################################################################
@@ -126,6 +130,7 @@ interval.cancer.time.k <- function(screen.times, k,
   #        k: k^{th} screen time, t_k
   #        lambda: mean pre-clinical onset time from exp(lambda)
   #        st_params: parameters for sojourn time function
+  #        pi: probability of being indolent, such that F*=pi+(1-pi)*F --> S*=(1-pi)S
   #        funs: function of sojourn time
   #        sensitivity: true sensitivity
   #        post.screen.lookout: post screen lookout time
@@ -139,7 +144,7 @@ interval.cancer.time.k <- function(screen.times, k,
     screen.times_i_1 <- ifelse(i == 1, 0, screen.times[i-1])
     interval.cancer <- interval.cancer + integrate(integrand.interval.cancer.false.negative,
                                                    lower=screen.times_i_1, upper=screen.times[i], 
-                                                   screen.times, k, post.screen.lookout, lambda, st_params, funs)$value * (1-sensitivity)^{(k-i+1)}
+                                                   screen.times, k, post.screen.lookout, lambda, st_params, pi, funs)$value * (1-sensitivity)^{(k-i+1)}
   }
   # case 2: interval detected in (t_k, t_{k+1})
   screen.times_k_1 <- screen.times[k] + post.screen.lookout
@@ -148,16 +153,15 @@ interval.cancer.time.k <- function(screen.times, k,
   # }
   interval.cancer <- interval.cancer + integrate(integrand.interval.cancer.true,
                                                  lower=screen.times[k], upper=screen.times_k_1, 
-                                                 screen.times, k, post.screen.lookout, lambda, st_params, funs)$value
+                                                 screen.times, k, post.screen.lookout, lambda, st_params, pi, funs)$value
   
   return(interval.cancer)
 }
 
 empirical.sensitivity.general <- function(screen.start.time, k, #start.time=NULL, end.time=NULL,
                                           sensitivity,
-                                          pre_onset_params, st_params, funs,
+                                          pre_onset_params, st_params, pi = 0, funs,
                                           post.screen.lookout,
-                                          # clinical.cancer.state, pre.clinical.cancer.state,
                                           method='Single'){
   ########################################################################################
   #Author: Yibai Zhao
@@ -167,6 +171,7 @@ empirical.sensitivity.general <- function(screen.start.time, k, #start.time=NULL
   #        k: k^{th} screen time, t_k
   #        sensitivity: true sensitivity
   #        rate.matrix=transition matrix of cancer model
+  #        pi: probability of being indolent, such that F*=pi+(1-pi)*F --> S*=(1-pi)S
   #        funs: function of sojourn time
   #        post.screen.lookout: post screen lookout time
   #        method: (1) 'Single': screen by screen at k^{th} screen time;
@@ -176,23 +181,24 @@ empirical.sensitivity.general <- function(screen.start.time, k, #start.time=NULL
 
   if( is.null(screen.start.time) |  (screen.start.time<=0) ) stop('screen start time must be greater than zero')
 
-  screen.times <- screen.start.time + cumsum(rep(post.screen.lookout, k)) - post.screen.lookout
-  
+  post.screen.lookout <- as.numeric(post.screen.lookout)
+  screen.times <- as.numeric(screen.start.time + cumsum(rep(post.screen.lookout, k)) - post.screen.lookout)
+  k <- as.numeric(k)
+  sensitivity <- as.numeric(sensitivity)
   lambda = pre_onset_params
 
-  screen.detect = screen.detect.time.k(screen.times, k, lambda, st_params, funs,
+  screen.detect = screen.detect.time.k(screen.times, k, lambda, st_params, pi, funs,
                                        sensitivity)
-  interval.cancer = interval.cancer.time.k(screen.times, k, lambda, st_params, funs,
+  interval.cancer = interval.cancer.time.k(screen.times, k, lambda, st_params, pi, funs,
                                            sensitivity,
                                            post.screen.lookout)
   
   if((method=='All') & (k>1)){ #& (1<start.time)){
-    # screen.detect <- interval.cancer <- 0
-    for (tk in c(2:k)) {
-    # for (tk in c(start.time:end.time)) {
-      screen.detect <- screen.detect + screen.detect.time.k(screen.times, k=tk, lambda, st_params, funs,
+    screen.detect <- interval.cancer <- 0
+    for (tk in c(1:k)) {
+      screen.detect <- screen.detect + screen.detect.time.k(screen.times, k=tk, lambda, st_params, pi, funs,
                                            sensitivity)
-      interval.cancer <- interval.cancer + interval.cancer.time.k(screen.times, k=tk, lambda, st_params, funs,
+      interval.cancer <- interval.cancer + interval.cancer.time.k(screen.times, k=tk, lambda, st_params, pi, funs,
                                                sensitivity,
                                                post.screen.lookout)
       
@@ -204,5 +210,13 @@ empirical.sensitivity.general <- function(screen.start.time, k, #start.time=NULL
 }
 
 
+#######################################
+# Find parameters for mst follows weibull distribution
+#######################################
+find_paramsF <- function(shape=NULL, scale=NULL, mst){
+  # shape = params[1]
+  # scale = params[2]
+  abs(scale*gamma(1+1/shape) - mst)
+}
 
 
