af5e13039f38580dc573f3e13fa9ac0608858530
yibaizhao <yibaizhao@umass.edu>
2021-12-08 15:09:43 -0800

    Add more examples

--- empirical_sensitivity_analytic_general_examples.R
+++ empirical_sensitivity_analytic_general_examples.R
@@ -15,6 +15,7 @@ library("ggplot2")
 library(reshape2)
 source("empirical_sensitivity_analytic_general_functions.R")
 source("simulation_functions_empirical_sensitivity_expanded.R")
+# source("empirical_sensitivity_analytic_timevarying_sens_general_functions.R")
 
 rateexample=matrix(c(-.002,.002,0,0,-.16,.16,0,0,0),byrow=T,nrow=3)
 
@@ -142,18 +143,16 @@ ggplot(data = dt_diff, aes(x = true_sensitivity, y = diff, color = k, group = k)
   scale_color_continuous(name = "kth screen")
   
 ###########
-# sojourn time follows weibull
+# sojourn time follows Weibull distribution
 ###########
 mst = 6
-find_paramsF <- function(params, mst){
-  shape = params[1]
-  scale = params[2]
-  abs(scale*gamma(1+1/shape) - mst)
-}
-st_params <- optim(par = c(0.1, 0.1), fn = find_paramsF, method = 'L-BFGS-B', lower = 0, upper = Inf, mst = mst)$par
+shape_incs = 2
+scale_incs <- optim(par = 0.1, fn = find_paramsF, method = 'L-BFGS-B', lower = 0, upper = Inf, shape = shape_incs, mst = mst)$par
+shape_decs = 0.5
+scale_decs <- optim(par = 0.1, fn = find_paramsF, method = 'L-BFGS-B', lower = 0, upper = Inf, shape = shape_decs, mst = mst)$par
 # shape = st_params[1]
 # scale = st_params[2]
-# mean(rweibull(100, shape = shape, scale = scale)) 
+# mean(rweibull(100, shape = shape, scale = scale))
 # scale*gamma(1+1/shape)
 
 # screen-by-screen 
@@ -185,11 +184,21 @@ empirical_analytic_weibull_out <-
   lapply(seq(.1,1,by=.01), 
          empirical.sensitivity.general,
          screen.start.time = 3, k = 1, #start.time=NULL, end.time=NULL,
-         pre_onset_params = 0.002, st_params = st_params, funs = 'weibull',
+         pre_onset_params = 0.002, st_params = c(shape_incs, scale_incs), funs = 'weibull',
          post.screen.lookout = 1,
          method='Single')
 lines(seq(.1,1,by=.01),unlist(empirical_analytic_weibull_out),xlab=c("true sensitvity"),ylab=c("empirical sensitvity"),type="l",col="green")
-legend("bottomright",legend=c("simulation_exp", "analytic_exp", "analytic_weibull", "y=x"),col=c("red","blue",'green',"black"),lty=1,lwd=1)
+
+empirical_analytic_weibull_out2 <-
+  lapply(seq(.1,1,by=.01), 
+         empirical.sensitivity.general,
+         screen.start.time = 3, k = 1, #start.time=NULL, end.time=NULL,
+         pre_onset_params = 0.002, st_params = c(shape_decs, scale_decs), funs = 'weibull',
+         post.screen.lookout = 1,
+         method='Single')
+lines(seq(.1,1,by=.01),unlist(empirical_analytic_weibull_out2),xlab=c("true sensitvity"),ylab=c("empirical sensitvity"),type="l",col="orange")
+legend("bottomright",legend=c("simulation_exp", "analytic_exp", "analytic_weibull_incs", "analytic_weibull_decs", "y=x"),col=c("red","blue",'green',"orange", "black"),lty=1,lwd=1)
+
 
 # Series of screen 
 # k=4
@@ -211,19 +220,78 @@ lines(seq(0,1),seq(0,1))
 empirical_analytic_exp_out <-
   lapply(seq(.1,1,by=.01), 
          empirical.sensitivity.general,
-         screen.start.time = 2, k = 5, #start.time=NULL, end.time=NULL,
+         screen.start.time = 1, k = 4, #start.time=NULL, end.time=NULL,
          pre_onset_params = 0.002, st_params = 1/mst, funs = 'exp',
          post.screen.lookout = 1,
          method='All')
 lines(seq(.1,1,by=.01),unlist(empirical_analytic_exp_out),xlab=c("true sensitvity"),ylab=c("empirical sensitvity"),type="l",col="blue")
 
+
 empirical_analytic_weibull_out <-
   lapply(seq(.1,1,by=.01), 
          empirical.sensitivity.general,
-         screen.start.time = 1, k = 4, #start.time=NULL, end.time=NULL,
-         pre_onset_params = 0.002, st_params = st_params, funs = 'weibull',
+         screen.start.time = 3, k = 1, #start.time=NULL, end.time=NULL,
+         pre_onset_params = 0.002, st_params = c(shape_incs, scale_incs), funs = 'weibull',
          post.screen.lookout = 1,
          method='All')
 lines(seq(.1,1,by=.01),unlist(empirical_analytic_weibull_out),xlab=c("true sensitvity"),ylab=c("empirical sensitvity"),type="l",col="green")
-legend("bottomright",legend=c("simulation_exp", "analytic_exp", "analytic_weibull", "y=x"),col=c("red","blue",'green',"black"),lty=1,lwd=1)
+
+empirical_analytic_weibull_out2 <-
+  lapply(seq(.1,1,by=.01), 
+         empirical.sensitivity.general,
+         screen.start.time = 3, k = 1, #start.time=NULL, end.time=NULL,
+         pre_onset_params = 0.002, st_params = c(shape_decs, scale_decs), funs = 'weibull',
+         post.screen.lookout = 1,
+         method='All')
+lines(seq(.1,1,by=.01),unlist(empirical_analytic_weibull_out2),xlab=c("true sensitvity"),ylab=c("empirical sensitvity"),type="l",col="orange")
+legend("bottomright",legend=c("simulation_exp", "analytic_exp", "analytic_weibull_incs", "analytic_weibull_decs", "y=x"),col=c("red","blue",'green',"orange", "black"),lty=1,lwd=1)
+
+###########
+# varing sensitivity over sojourn time
+###########
+# screen-by-screen 
+# k=1
+## simulation method
+empirical_out_simulation_V1=lapply(seq(.1,1,by=.1),empirical.sensitivity.simulation,rate.matrix=rateexample,
+                                   start.dist=c(1,0,0),
+                                   screen.times=c(3),
+                                   post.screen.lookout=1,
+                                   clinical.cancer.state=3,
+                                   pre.clinical.cancer.state=2,
+                                   nreps=10000)
+plot(seq(.1,1,by=.1),unlist(empirical_out_simulation_V1),xlab=c("true sensitvity"),ylab=c("empirical sensitvity"),type="l",col="red",xlim=c(0,1),
+     ylim=c(0,1), 
+     main = "Empirical versus true sensitivity\nScreen by screen at time=3")
+lines(seq(0,1),seq(0,1))
+
+
+## analytic method
+empirical_analytic_out <-
+  lapply(seq(.1,1,by=.01), 
+         empirical.sensitivity.general.V2,
+         screen.start.time = 3, k = 1, #start.time=NULL, end.time=NULL,
+         sens.time.varying = FALSE,
+         pre_onset_params = 0.002, st_params = 0.16, funs = 'exp',
+         post.screen.lookout = 1,
+         method='Single')
+lines(seq(.1,1,by=.01),unlist(empirical_analytic_out),xlab=c("true sensitvity"),ylab=c("empirical sensitvity"),type="l",col="blue")
+legend("bottomright",legend=c("analytic", "simulation","y=x"),col=c("blue","red","black"),lty=1,lwd=1)
+
+
+## analytic method and time-varying
+true_sens_time_varying <- sensF(seq(0,10,length.out=100), sensitivity=NULL, sens.time.varying=TRUE, sens_params=c(-0.5,0.5), dist = 'linear')
+plot(seq(0,10,length.out=100),true_sens_time_varying,xlab=c("time"),ylab=c("sensitvity"),type="l",col="red",xlim=c(0,10),
+     ylim=c(0,1), 
+     main = "Empirical versus true sensitivity varying over time")
+empirical_time_varying_analytic_out <-
+  sapply(seq(1,10,1), function(k)
+         empirical.sensitivity.general.V2(
+         screen.start.time = 1, k=k, #start.time=NULL, end.time=NULL,
+         sensitivity = NULL,
+         sens.time.varying = TRUE,
+         pre_onset_params = 0.002, st_params = 0.16, sens_params = c(-0.5, 0.5), funs = 'exp',
+         post.screen.lookout = 1,
+         method='Single'))
+lines(seq(1,10,1),unlist(empirical_time_varying_analytic_out),xlab=c("true sensitvity"),ylab=c("empirical sensitvity"),type="l",col="blue")
+legend("bottomright",legend=c("empirical sens with time varying", "true sensitivity"),col=c("blue","red"),lty=1,lwd=1)
 
